05ba8e809c031d56adb1854a728d47fb19ed1bec
IamGiel <november14giel@gmail.com>
2019-07-11 18:06:59 -0400

    adding heat map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,7 @@ export class AppComponent {
       private mapService: FirebaseService
       ) {}
 
-    ngOnInit() {
-    }
-    
-    
+    ngOnInit() {}
     ngAfterContentInit() {
 
         this.mapService.getCutomerOrder().subscribe(data => {
@@ -290,13 +287,130 @@ export class AppComponent {
                     .selectAll('text')
                     .attr('transform', 'rotate(-35)')
                     .attr('text-anchor', 'start');
-                
             };
            
             update2ndGraph(this.HealthData);
         });
 
-}
+        // ========== service fetch another data for HORIZONTAL bar chart =============
+        this.mapService.getCutomerStat().subscribe(data => {
+          let xdata = data.map(e => {
+              return { ...e.payload.doc.data() };
+          });
+
+          // console.log(xdata);
+          this.HealthData = xdata;
+
+          // select svg container
+          const svg2 = d3.select('#graphContainer2');
+
+          const margin = { top: 100, right: 100, bottom: 20, left: 100 };
+          const graphWidth = 600 - margin.left - margin.right;
+          const graphHeight = 600 - margin.top - margin.bottom;
+
+          // append the svg object to the body of the page
+          const svg = d3.select("#heat-map")
+          .append("svg")
+            .attr("width", graphWidth + margin.left + margin.right)
+            .attr("height", graphHeight + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+                const graph3 = svg2
+                .append('g')
+                .attr('width', graphWidth)
+                .attr('height', graphHeight)
+                .attr('transform', `translate( ${margin.left}, ${margin.top})`);
+
+                // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+                const xAxes = d3.map(this.CustomerData, function(d){return d.priority;}).keys()
+                const yAxes = d3.map(this.CustomerData, function(d){return d.name;}).keys()
+
+                // Build X scales and axis:
+                const x = d3.scaleBand()
+                  .range([ 0, graphWidth ])
+                  .domain(xAxes)
+                  .padding(0.05);
+                svg.append("g")
+                  .style("font-size", 15)
+                  .attr("transform", "translate(0," + graphHeight + ")")
+                  .call(d3.axisBottom(x).tickSize(0))
+                  // .select(".domain").remove()
+
+                // Build Y scales and axis:
+                const y = d3.scaleBand()
+                  .range([ graphHeight, 0 ])
+                  .domain(yAxes)
+                  .padding(0.05);
+                svg.append("g")
+                  .style("font-size", 15)
+                  // .attr("transform", "translate(0," + graphHeight + ")")
+                  .call(d3.axisLeft(y).tickSize(0))
+                  // .select(".domain").remove()
+
+                // Build color scale
+                const myColor = d3.scaleLinear<string>()
+                  .domain([50, 1000])
+                  .range(["red", "green"]);
+
+                // select tool tip div
+                const hmToolTip = d3.select("#heatmap-tooltip")
+                  // .append("div")
+                  .style("opacity", 0)
+                  .style("position", "relative")
+                  .attr("class", "tooltip")
+                  // .style("background-color", "white")
+                  // .style("border", "solid")
+                  // .style("border-width", "2px")
+                  // .style("border-radius", "5px")
+                  .style("padding", "5px")
+
+                // Three function that change the tooltip when user hover / move / leave a cell
+                const mouseover = function(d) {
+                  hmToolTip
+                    .style("opacity", 1)
+                  d3.select(this)
+                    .style("stroke", "yellow")
+                    .style("opacity", 1)
+                }
+                const mousemove = function(d) {
+                  hmToolTip
+                    .html(`${d.name} has ${d.orders} orders with priority level: ${d.priority}`)
+                    .style("left", (d3.mouse(this)[0]-200) + "px")
+                    .style("top", (d3.mouse(this)[1]+40) + "px")
+                    .style("color", "white")
+                }
+                const mouseleave = function(d) {
+                  hmToolTip
+                    .style("opacity", 0)
+                  d3.select(this)
+                    .style("stroke", "none")
+                    .style("opacity", 0.8)
+                }
+
+                // add the squares
+                svg.selectAll()
+                .data(this.CustomerData, function(d) {return d.name+':'+d.orders;})
+                .enter()
+                .append("rect")
+                  .attr("x", function(d) { return x(d.priority) }) // allows to spread the square diagonally across the chart
+                  .attr("y", function(d) { return y(d.name) }) // allows to spread the square diagonally across the chart
+                  .attr("rx", 4)
+                  .attr("ry", 4)
+                  .attr("width", x.bandwidth() )
+                  .attr("height", y.bandwidth() )
+                  .style("fill", function(d) { return myColor(d.orders)} )
+                  .style("stroke-width", 4)
+                  .style("stroke", "none")
+                  .style("opacity", 0.8)
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave)
+            
+          })
+
+    }
 
     handleMouseEventsOn = (d, i, n) => {
         console.log(n[i]);
