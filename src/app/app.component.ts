4c3acdcadb356cc4d9e9dff442f42ecefdbcf5a0
IamGiel <november14giel@gmail.com>
2019-07-25 10:52:43 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, ElementRef, ComponentFactoryResolver } from '@angular/core';
 import * as d3 from 'd3';
-import "../"
 
 import { FirebaseService } from './service/firebase.service';
 import { MyTooltipComponentComponent } from './my-tooltip-component/my-tooltip-component.component';
@@ -288,52 +287,188 @@ export class AppComponent {
           .on("mouseleave", mouseleave)
             
           })
-          // ==========@@@@@@@@@@ service fetch another data for HEXBIN MAP chart @@@@@@@@@@@@=============
 
-          d3.csv("https://iamgiel.github.io/resume/assets/world3.csv").then((data)=>{ 
-                  console.log(data)
-                  this.CustomerData = data;
-                // set the dimensions and margins of the graph
-                const margin = {top: 10, right: 30, bottom: 30, left: 40};
-                const width = 460 - margin.left - margin.right;
-                const height = 400 - margin.top - margin.bottom;
+          // ==========@@@@@@@@@@ service fetch another data for SCATTER PLOT MAP chart @@@@@@@@@@@@=============
 
-                // append the svg object to the body of the page
-                const svg = d3.select("#hexbin")
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform",
-                      "translate(" + margin.left + "," + margin.top + ")");
+          d3.csv("https://iamgiel.github.io/resume/assets/world3.csv").then((data2)=>{ 
+          // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv").then((data2) => {
+            console.log(data2)
+            let data = data2;
 
-                // Add X axis
-                const x = d3.scaleLinear()
-                  .domain([5, 18])
-                  .range([ 0, width ]);
-                svg.append("g")
-                  .attr("transform", "translate(0," + height + ")")
-                  .call(d3.axisBottom(x)); 
-                // Add Y axis
-              const y = d3.scaleLinear()
-              .domain([5, 20])
-              .range([ height, 0 ]);
-              svg.append("g")
-                .call(d3.axisLeft(y));
-            // Reformat the data: d3.hexbin() needs a specific format
-            const inputForHexbinFun = []
-            this.CustomerData.forEach(function(d) {
-              inputForHexbinFun.push( [x(d.x), y(d.y)] )  // Note that we had the transform value of X and Y !
-            })
-            // Prepare a color palette
-            const color = d3.scaleLinear<any>()
-            .domain([0, 600]) // Number of points in the bin?
-            .range(["transparent",  "#69b3a2"])
+            
+            let max = d3.max(data, function(d){
+              return parseFloat(d["Population Total"])
+            }); // maximum of x and y
+              console.log(max)
 
-            // Compute the hexbin data
-            const hexbin = d3.h ()
-            .radius(11) // size of the bin in px
-            .extent([ [0, 0], [width, height] ])
+            let colorDomainMin = d3.min(data, function(d){
+              let yearString = `${d["Year"]}`;
+              let year = `${yearString.substring(5)}`;
+              return year ? year : "2000";
+            }); // maximum of x and y
+              console.log(colorDomainMin)
+
+            let colorDomainMax = d3.max(data, function(d){
+              return d["Year"].substring(5);
+            }); // maximum of x and y
+              console.log(colorDomainMax)
+
+            // dimensions and margins
+            var margin = {top: 100, right: 40, bottom: 100, left: 150},
+            width = 1200 - margin.left - margin.right,
+            height = 1200 - margin.top - margin.bottom;
+
+            // Pan and zoom
+            var zoom = d3.zoom()
+                .scaleExtent([.1, 50])
+                .extent([[0, 0], [width, height]])
+                .on("zoom", zoomed);
+
+            // append the svg object to the body of the page
+            var svg = d3.select("#scatterPlot")
+            .append("svg")
+            .attr("class", "scatterPlotDimension")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .call(zoom)
+            .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+                  // LEGEND
+          // select the svg area
+          // const legendSvg = d3.select("#legend")
+          // Handmade legend
+          // svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#4BA15E").attr("transform", "translate(200,-100)")
+          svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#A7CDD9").attr("transform", "translate(0,-220)")
+          svg.append("text").attr("x", 220).attr("y", 130).text("Male Life Expectency (Age)").style("font-size", "20px").attr("alignment-baseline","middle").attr("transform", "translate(200,-100)")
+          svg.append("text").attr("x", -400).attr("y", 30).text("CO2 Emmissions").style("font-size", "20px").attr("alignment-baseline","middle").attr("transform", "translate(-300,100)").attr("transform", "rotate(-90)")
+
+
+            const spToolTip = d3.select("#scatterTooltip")
+            .append("div")
+            .style("opacity", 0)
+            .style("position", "relative")
+            // .attr("class", "tooltip")
+            .style("padding", "5px")
+  
+  
+              // Three function that change the tooltip when user hover / move / leave a cell
+              const mouseover = function(d) {
+                let yearString = `${d["Year"]}`;
+                // let life = d["Country"] ? d["Life Expectancy Male"] : "n/a";
+                let country = `${d["Country"] ? d["Country"] : "n/a"}`;
+                let population = `${d["Population Total"]}`;
+                let year =  `${d["Year"] ? d["Year"] : "n/a"}`;
+                let x = `${d3.mouse(this)[0]}px`;
+                console.log(x)
+                console.log(`Country: ${country}<br> Population: ${population}`)
+                d3.select(this)
+                .style("stroke", "red")
+                .style("fill", "white")
+                .style("opacity", 0.8)
+                .style("cursor", "pointer"); 
+                spToolTip
+                .style("opacity", 1)
+                .style("position", "relative")
+                // .attr("class", "tooltip")
+                .style("padding", "5px")
+                  .html(`Country: ${country} <br> Recorded Year: ${year}`)
+                  // .style("left", (d3.mouse(this)[0]-450) + "px")
+                  // .style("top", (d3.mouse(this)[1]+100) + "px")
+                  .style("left", (d3.mouse(this)[0]-350) + "px")
+                  .style("top", (d3.mouse(this)[1]+160) + "px")
+                  .style("color", "white")
+              }
+              const mousemove = function(d) {
+              
+              }
+              const mouseleave = function() {
+                console.log("mouseout")
+                spToolTip
+                  .style("opacity", 0)
+                d3.select(this)
+                  .style("stroke", "white")
+                  .style("opacity", 0.8)
+                  .style("fill", "#A694E8")
+                  .style("cursor", "default"); 
+              }
+
+            // create scale objects
+            var xScale = d3.scaleLinear()
+              .domain([0, 100])
+              .range([0, width]);
+            var yScale = d3.scaleLinear()
+              .domain([0, 1000])
+              .range([height, 0]);
+            // create axis objects
+            var xAxis = d3.axisTop(xScale)
+              .ticks(20, "s");
+            var yAxis = d3.axisLeft(yScale)
+              .ticks(20, "s");
+            // Draw Axis
+            var gX = svg.append('g')
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+              .call(xAxis);
+            var gY = svg.append('g')
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+              .call(yAxis);
+
+             // Add a clipPath: everything out of this area won't be drawn.
+            var clip = svg.append("defs").append("SVG:clipPath")
+            .attr("id", "clip")
+            .append("SVG:rect")
+            .attr("width", width )
+            .attr("height", height )
+            .attr("x", 0)
+            .attr("y", 0);
+
+            // Color scale: give me a specie name, I return a color
+            var color = d3.scaleOrdinal()
+            .domain(["10", "100"])
+            .range([ "white", "pink", "brown", "red", "orange", "purple", "blue", "green", "lightgreen", "lightred", "lightorange", "black"])
+
+            // Draw Datapoints
+            var points_g = svg.append("g")
+              .attr("clip-path", "url(#clip)")
+              .attr("width", width)
+              .attr("height", height)
+              .style("fill", `${color}`)
+              .style("pointer-events", "visibleFill")
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+              
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+              .attr("clip-path", "url(#clip)")
+              .classed("points_g", true)
+              .call(zoom)
+           
+            var points = points_g.selectAll("circle")
+              .data(data);
+              console.log(data)
+            points = points.enter().append("circle")
+              .attr('cx', function(d) {return xScale(parseFloat(d["Life Expectancy Male"]) ? parseFloat(d["Life Expectancy Male"]) : 0)})
+              .attr('cy', function(d) {return yScale(parseInt(d["CO2 Emissions"]) ? parseInt(d["CO2 Emissions"]) : 0)})
+              .attr('r', 10)
+              
+              .style("fill", `${color}`)
+              .style("opacity", 0.3)
+              .style("stroke", `${color}`)
+              .style("pointer-events", "visibleFill")
+              .on("mouseover", mouseover)
+              .on("mousemove", mousemove)
+              .on("mouseleave", mouseleave)
+
+            function zoomed() {
+            // create new scale ojects based on event
+                var new_xScale = d3.event.transform.rescaleX(xScale);
+                var new_yScale = d3.event.transform.rescaleY(yScale);
+            // update axes
+                gX.call(xAxis.scale(new_xScale));
+                gY.call(yAxis.scale(new_yScale));
+                points.data(data)
+                .attr('cx', function(d) {return new_xScale(parseInt(d["Life Expectancy Male"]) ? parseInt(d["Life Expectancy Male"]) : 0)})
+                .attr('cy', function(d) {return new_yScale(parseInt(d["CO2 Emissions"]) ? parseInt(d["CO2 Emissions"]) : 0)})
+            }
           })
     }
 
